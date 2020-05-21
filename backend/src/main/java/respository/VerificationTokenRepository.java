685cf6aeaa405dea403b1592a6c47f09b69aa7f5
saviorocha <44814299+saviorocha@users.noreply.github.com>
2020-05-21 20:36:17 -0300

    Corrigido erro no nome dos objetos

--- backend/src/main/java/respository/VerificationTokenRepository.java
+++ backend/src/main/java/respository/VerificationTokenRepository.java
@@ -1,9 +1,7 @@
 package respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import model.VerificationToken;
-
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long>{
 
 }
